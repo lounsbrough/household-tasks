bd2a82f85e9230553eee23c08a734a40bd9e5ac8
David Lounsbrough <david.lounsbrough@gmail.com>
2018-10-27 11:35:54 -0500

    Organizing

--- includes/header.php
+++ includes/header.php
@@ -19,12 +19,12 @@ session_regenerate_id();
         <meta name="description" content="Household Tasks">
         <meta name="author" content="David Lounsbrough">
 
-        <link rel="icon" type="image/png" href="favicon-32x32.png">
-        <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16">
-        <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32">
-        <link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96">
-        <link rel="icon" type="image/png" href="favicon-192x192.png" sizes="192x192">
-        <link rel="shortcut icon" href="favicon.ico">
+        <link rel="icon" type="image/png" href="favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" href="favicon/favicon-16x16.png" sizes="16x16">
+        <link rel="icon" type="image/png" href="favicon/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="favicon/favicon-96x96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="favicon/favicon-192x192.png" sizes="192x192">
+        <link rel="shortcut icon" href="favicon/favicon.ico">
 
         <title><?php echo $pageTitle; ?></title>
 
